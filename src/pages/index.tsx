99f43c5afd557ba861d9c237b4f4d6726a49cab7
Ardeman <mail@ardeman.com>
2023-04-21 11:14:39 +0700

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ import { UserType } from "@/types/user";
 import Switcher from "@/components/Switcher";
 import Card from "@/components/Card";
 import Spinner from "@/components/Spinner";
+import Head from "next/head";
 
 export default function Home() {
+  const title = "Daftar Pengguna";
   const [view, setView] = useState<"list" | "grid">("list"); // default view is list
   const { isLoading, error, data } = useQuery<UserType[]>("users", () =>
     axios
@@ -16,12 +18,17 @@ export default function Home() {
 
   return (
     <div className="container px-2 md:px-0 mx-auto py-16 min-h-screen">
+      <Head>
+        <title>{title}</title>
+      </Head>
       <div className="grid gap-8">
         <h1 className="text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-          Daftar Pengguna
+          {title}
         </h1>
         {error ? (
-          <p className="text-center">An error has occurred: {(error as AxiosError).message}</p>
+          <p className="text-center">
+            An error has occurred: {(error as AxiosError).message}
+          </p>
         ) : isLoading ? (
           <Spinner />
         ) : (
@@ -31,7 +38,9 @@ export default function Home() {
             </div>
             <div
               className={`${
-                view === "list" ? "flex flex-col" : "grid grid-cols-2 md:grid-cols-3"
+                view === "list"
+                  ? "flex flex-col"
+                  : "grid grid-cols-2 md:grid-cols-3"
               } gap-4`}
             >
               {data?.map((user) => (
